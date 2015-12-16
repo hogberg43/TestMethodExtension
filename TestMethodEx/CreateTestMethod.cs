b59e24fdb276da4d205d1a6e9d07fd795fdb1098
hogberg43 <mhogberg@sbcglobal.net>
2015-12-16 08:39:09 -0500

    Added support for multiple method composition and for the automatic selection of the line that the cursor is on.

--- TestMethodEx/CreateTestMethod.cs
+++ TestMethodEx/CreateTestMethod.cs
@@ -1,17 +1,9 @@
-﻿//------------------------------------------------------------------------------
-// <copyright file="CreateTestMethod.cs" company="Company">
-//     Copyright (c) Company.  All rights reserved.
-// </copyright>
-//------------------------------------------------------------------------------
-
-using System;
+﻿using System;
 using System.ComponentModel.Design;
-using System.Globalization;
 using System.Text;
 using EnvDTE;
 
 using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
 
 namespace TestMethodEx
 {
@@ -21,6 +13,8 @@ namespace TestMethodEx
     internal sealed class CreateTestMethod
     {
         private DTE dte;
+
+        #region VS template code
         /// <summary>
         /// Command ID.
         /// </summary>
@@ -87,7 +81,13 @@ namespace TestMethodEx
         {
             Instance = new CreateTestMethod(package);
         }
+        #endregion
 
+        /// <summary>
+        /// Creates the test method to replace the text being converted.
+        /// </summary>
+        /// <param name="selection">Text selection in the current document</param>
+        /// <returns></returns>
         string CreateMethodFromText(string selection)
         {
             if (!selection.Contains("\r\n") && selection.Length > 0)
@@ -95,31 +95,66 @@ namespace TestMethodEx
                 var sb = new StringBuilder();
                 sb.Append("[TestMethod]\r\n");
                 sb.Append("public void ");
-                sb.Append(selection.Replace(" ", "_").Replace("\"","").Replace("//",""));
+                sb.Append(selection.Trim().Replace(" ", "_").Replace("\"","").Replace("//",""));
                 sb.Append("() \r\n{\r\n");
                 sb.Append("// Arrange\r\n\r\n\r\n");
                 sb.Append("// Act\r\n\r\n\r\n");
                 sb.Append("// Assert\r\n");
-                //sb.Append("}");
-
+                sb.Append("throw new NotImplementedException();\r\n"); // ensure that new test method fails (Red, Green, Refactor)
                 selection = sb.ToString();
             }
             return selection;
         }
 
+        /// <summary>
+        /// Custom command implementation
+        /// </summary>
         private void MenuItemCallback(object sender, EventArgs e)
         {
-            this.dte = (DTE)ServiceProvider.GetService(typeof(DTE));
+            // Initialize COM library containing references to the current text editor
+            dte = (DTE)ServiceProvider.GetService(typeof(DTE));
+
+            // ensure there is an active document open before processing
             if (dte.ActiveDocument != null)
             {
                 var doc = (TextDocument)dte.ActiveDocument.Object("TextDocument");
                 if (doc != null)
                 {
+
+                    // Ensure that the current document is an MSTest class
                     var startPoint = doc.StartPoint.CreateEditPoint();
                     var isTestClass = startPoint.GetText(doc.EndPoint).Contains("[TestClass]");
                     if (isTestClass)
                     {
-                        doc.Selection.Text = CreateMethodFromText(doc.Selection.Text);
+                        // Test for a current selection
+                        var selectedText = doc.Selection.Text;
+                        if (selectedText.Length == 0)
+                        {
+
+                            // use cursor location to select current line and create method
+                            // if no selection has been made
+                            var line_index = doc.Selection.ActivePoint.Line;
+                            var edit_point = doc.CreateEditPoint();
+                            var line = edit_point.GetLines(line_index, line_index + 1).Trim();
+
+
+                            // select the entire line and replace with the method stub
+                            doc.Selection.StartOfLine(0);
+                            doc.Selection.EndOfLine(true);
+                            doc.Selection.Text = CreateMethodFromText(line); 
+
+                        }
+                        else
+                        {
+                            // use the current selection to create a new method or methods if more than one line is selected
+                            var lines = doc.Selection.Text.Split(new[] {"\r\n"}, StringSplitOptions.None);
+                            var output = "";
+                            foreach (var line in lines)
+                            {
+                                output += CreateMethodFromText(line) + "}\r\n";
+                            }
+                            doc.Selection.Text = output;
+                        }
                     }
                 }
             }
